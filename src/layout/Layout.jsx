d9dde3e238e375bdeb37b8dfd9e27e16b1a15aa7
ShroukMagdy3 <shorokmagdy15@gmail.com>
2026-05-15 17:52:56 +0300

    fix container in mobile screen

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -5,7 +5,7 @@ export default function Layout() {
   return (
     <div className="min-h-screen bg-black ">
       <Navbar />
-      <main className="max-w-6xl mx-auto px-4 py-8">
+      <main className="sm:max-w-6xl sm:mx-auto sm:px-4 py-8">
         <Outlet />
       </main>
     </div>
